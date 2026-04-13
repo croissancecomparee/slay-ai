c9f4328010b2408ef097d7959aed7ca76d496227
croissancecomparee <gaetan.cece.90@gmail.com>
2026-04-14 01:43:34 +0200

    creation d'api pour récupérer une carte par son nom

--- app/services/card_service/card_api.py
+++ app/services/card_service/card_api.py
@@ -1,5 +1,20 @@
 import json
 
 def get_all_cards():
+    '''
+    Cette fonction lit le fichier JSON contenant les cartes et retourne une liste de dictionnaires représentant les cartes.
+    '''
     with open("app/data/cards.json") as f:
         return json.load(f)
+    
+def get_card_by_name(name: str):
+    '''
+    On récupère toutes les cartes, puis on parcourt la liste pour trouver la carte dont le nom correspond à celui donné en paramètre.
+    Si on trouve la carte, on la retourne, 
+    sinon on retourne None.
+    '''
+    cards = get_all_cards()
+    for card in cards:
+        if card["name"].lower() == name.lower():
+            return card
+    return None
