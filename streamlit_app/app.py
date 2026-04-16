887046ecbd588bd41fe9f7fee152febff93107f2
croissancecomparee <gaetan.cece.90@gmail.com>
2026-04-17 00:12:22 +0200

    add multiselect with list for select card

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -4,14 +4,46 @@ import pandas as pd
 
 st.title("Slay The Spire Deck Analyzer")
 
+# oour le moment on ne fait rien avec le personnage, mais on pourrait l'utiliser pour filtrer les cartes disponibles ou pour donner des conseils spécifiques à ce personnage.
+selected_character = st.selectbox(
+    "Choose character",
+    ["ironclad", "silent"]
+)
+
 # input for the deck
-deck_input = st.text_area("Enter your deck (one card name per line)" \
-"separated by commas" \
-"Strike, Defend, Bash, etc.")
+response = requests.get("http://localhost:8000/cards")
+cards = response.json()
+
+card_names = [c["name"] for c in cards]
+
+# on choisi le nombre de cartes:
+deck_dict = {}
+
+st.subheader("Build your deck")
+
+for card in card_names:
+    count = st.number_input(
+        f"{card}",
+        min_value=0,
+        max_value=10,
+        step=1,
+        key=card
+    )
+    if count > 0:
+        deck_dict[card] = count
+
+deck = []
+for card, count in deck_dict.items():
+    deck.extend([card] * count)
+# deck = st.multiselect("Select cards for your deck", card_names)
+# deck_input = st.text_area("Enter your deck (one card name per line)\n" \
+# "separated by commas\n" \
+# "Strike, Defend, Bash, etc.")
 
 if st.button("Analyze Deck"):
+
     # split the input into a list of card names
-    deck = [card.strip() for card in deck_input.split(",")]
+    # deck = [card.strip() for card in deck_input.split(",")]
 
     # send the deck to the API for analysis
     response = requests.post("http://localhost:8000/deck/analyze", json=deck)
